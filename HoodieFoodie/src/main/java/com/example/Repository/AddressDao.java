3536f25710c1da37fa40b02f41e2a251002c16dc
Sagar Pramanik <sgrprmnk@gmail.com>
2022-12-18 01:20:57 +0530

    Delete HoodieFoodie/src/main/java/com/example/Repository directory

--- HoodieFoodie/src/main/java/com/example/Repository/AddressDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressDao extends JpaRepository<Address,String> {
-    public Address findByArea(String area);
-}
